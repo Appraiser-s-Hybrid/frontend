e9aed6b056670a9d530cd9ddf172703cb7585dfa
Jeffreyo3 <Jeffrey.Orndorff@gmail.com>
2019-10-17 20:01:34 -0700

    Created validation using Yup

--- src/components/OnLogin.js
+++ src/components/OnLogin.js
@@ -3,7 +3,7 @@ import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 
-const OnLogin = () => {
+const OnLogin = ({ values, errors, touched, status }) => {
     const [params, setParams] = useState([]);
     const [param, setParam] = useState('');
     const [search, setSearch] = useState('');
@@ -26,19 +26,47 @@ const OnLogin = () => {
         setParam('');
     };
 
+    useEffect(() => {
+        if (status) {
+            setParams([...params, status]);
+        }
+    }, [status]);
+
 
     return (
         
         <div>
-            <Form className="user-search">
+            <Form className="user-form">
+                <label>
+                    Square Feet: 
+                <Field type="number" name="sqft" placeholder="Square Feet" />
+                {touched.sqft && errors.sqft &&(
+                    <p className="error">{errors.sqft}</p>
+                )}
+                </label>
+
                 <label>
                     Zip Code: 
                 <Field type="number" name="zip" placeholder="Zip Code" />
+                {touched.zip && errors.zip &&(
+                    <p className="error">You must enter a Zip Code in order to receive results!</p>
+                )}
                 </label>
 
                 <label>
                     Number of Rooms: 
-                <Field type="number" name="rooms" placeholder="Number of Bedrooms" />
+                <Field type="number" name="rooms" placeholder="Numbe of Rooms" />
+                {touched.rooms && errors.rooms &&(
+                    <p className="error">{errors.rooms}</p>
+                )}
+                </label>
+
+                <label>
+                    Number of Bathrooms: 
+                <Field type="number" name="bathrooms" placeholder="Number of Bedrooms" />
+                {touched.bathrooms && errors.bathrooms &&(
+                    <p className="error">{errors.bathrooms}</p>
+                )}
                 </label>
 
                 <label>
@@ -47,22 +75,36 @@ const OnLogin = () => {
                 </label>
                 <button type="submit">SEARCH</button>
             </Form>
+            <div>
+                {params.map(param => (
+                    <p>{param.zip}</p>
+                ))}
+            </div>
         </div>
-        
     );
 };
 
 const FormikUserSearch = withFormik({
-    mapPropsToValues({ zip, rooms, TBD }) {
+    mapPropsToValues({ sqft, zip, rooms, bathrooms, TBD }) {
         return {
+            sqft: sqft || "",
             zip: zip || "",
             rooms: rooms || "",
+            bathrooms: bathrooms || "",
             TBD: TBD || ""
         };
     },
 
+    validationSchema: Yup.object().shape({
+        sqft: Yup.number(),
+        zip: Yup.number().required(),
+        rooms: Yup.number(),
+        bathrooms: Yup.number(),
+        TBD: Yup.mixed()
+    }),
+
     handleSubmit(values, { setStatus }) {
-    
+        setStatus(values);
     }
     
 })(OnLogin);
